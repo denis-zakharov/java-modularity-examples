f265ad99296716f30a89606e92e14a03e0bcd6bf
Denis Zakharov <dizaharov@gmail.com>
2019-02-23 22:34:54 +0300

    jaxb jars

--- chapter7/jaxb/run.sh
+++ chapter7/jaxb/run.sh
@@ -1,2 +1,4 @@
-$JAVA_HOME/bin/javac -d out --add-modules java.xml.bind -sourcepath src $(find src -name '*.java')
-$JAVA_HOME/bin/java -cp out --add-modules java.xml.bind example.JaxbExample
+# ls mods/
+# javax.activation-api-1.2.0.jar  jaxb-api.jar  jaxb-core.jar  jaxb-impl.jar  jaxb-jxc.jar  jaxb-xjc.jar
+$JAVA_HOME/bin/javac -d out -cp "mods/*" -sourcepath src $(find src -name '*.java')
+$JAVA_HOME/bin/java -cp "out:mods/*" --add-opens java.base/java.lang=ALL-UNNAMED example.JaxbExample
