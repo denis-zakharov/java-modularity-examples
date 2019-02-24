c46090ab3f46dfcd35f6d7560316030399778dd2
Denis Zakharov <dizaharov@gmail.com>
2019-02-24 23:54:43 +0300

    commit jaxb impl jars

--- chapter7/jaxb/run.sh
+++ chapter7/jaxb/run.sh
@@ -1,4 +1,4 @@
 # ls mods/
 # javax.activation-api-1.2.0.jar  jaxb-api.jar  jaxb-core.jar  jaxb-impl.jar  jaxb-jxc.jar  jaxb-xjc.jar
-$JAVA_HOME/bin/javac -d out -cp "mods/*" -sourcepath src $(find src -name '*.java')
-$JAVA_HOME/bin/java -cp "out:mods/*" --add-opens java.base/java.lang=ALL-UNNAMED example.JaxbExample
+$JAVA_HOME/bin/javac -d out -cp "lib/*" -sourcepath src $(find src -name '*.java')
+$JAVA_HOME/bin/java -cp "out:lib/*" --add-opens java.base/java.lang=ALL-UNNAMED example.JaxbExample
